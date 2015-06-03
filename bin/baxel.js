d9777d4bd9f41e9df61f3c1822ab5a2d3b0a4fed
Norman Paniagua <normanpaniagua@gmail.com>
2015-06-03 14:21:58 -0400

    Added bin folder, that for some issue on the gitignore was not included

---
+++ bin/baxel.js
@@ -0,0 +1,167 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+  , program = require('commander')
+  , path = require('path')
+  , mkdirp = require('mkdirp')
+  , AdmZip = require('adm-zip')
+  , http = require('http')
+  , url = require('url')
+  , nodemon = require('nodemon')
+//  , templateURL = require('../package.json')['baxel-template'];
+  , templateURL = 'https://github.com/request/request/archive/v2.57.1.zip';
+
+// Baxel options
+
+var options = {};
+
+// options
+
+program
+  .version(require('../package.json').version)
+  .usage('[options]')
+  .option('run', 'run Baxel project on the current directory')
+  .option('new [name]', 'creates a new project on the folder with the same project name');
+
+program.on('--help', function(){
+  console.log('  Usage:');
+  console.log('');
+  console.log('    # create a new project');
+  console.log('    $ baxel new blog');
+  console.log('');
+  console.log('    # baxel run');
+  console.log('    $ run the current baxel project');
+  console.log('');
+});
+
+program.parse(process.argv);
+
+// options given, parse them
+
+if (program.obj) {
+  options = parseObj(program.obj);
+}
+
+/**
+ * Parse object either in `input` or in the file called `input`. The latter is
+ * searched first.
+ */
+function parseObj (input) {
+  var str, out;
+  try {
+    str = fs.readFileSync(program.obj);
+  } catch (e) {
+    return eval('(' + program.obj + ')');
+  }
+  // We don't want to catch exceptions thrown in JSON.parse() so have to
+  // use this two-step approach.
+  return JSON.parse(str);
+}
+
+// run
+
+function _run () {
+  nodemon({
+    script: 'app.js',
+    ext: 'js json'
+  });
+  
+  nodemon.on('start', function () {
+    console.log('Baxel development server started');
+  }).on('quit', function () {
+    console.log('Baxel development has quit');
+  }).on('restart', function (files) {
+    console.log('Baxel development restarted due to: ', files);
+  });
+}
+
+// new
+
+function _new () {
+  if (program.new == undefined || program.new.length == 0) {
+    throw new Error('Please specify a project name');
+    return;
+  }
+  
+  var options = {
+      host: url.parse(templateURL).host,
+      port: 80,
+      path: url.parse(templateURL).pathname,
+      agent: false
+  };
+  
+  console.log(options);
+
+  http.get(options, function(res) {
+      var data = [], dataLen = 0; 
+    
+      console.log('Wait until the Baxel Hamel is downloaded');
+    
+      res.on('data', function(chunk) {
+  
+              data.push(chunk);
+              dataLen += chunk.length;
+  
+          }).on('end', function() {
+              var buf = new Buffer(dataLen);
+  
+              for (var i=0, len = data.length, pos = 0; i < len; i++) { 
+                  data[i].copy(buf, pos); 
+                  pos += data[i].length; 
+              }
+              
+              var zip = new AdmZip(buf)
+                , entries = zip.getEntries();
+                
+              zip.extractAllTo (process.cwd() + '/', true);
+              console.log(entries);
+              
+              console.log('Baxel Hamet finished, now enter the project folder "' + program.new + '" and start hacking');
+          });
+  });
+}
+
+function _new2 () {
+  var request = require('request');
+  var data = [], dataLen = 0; 
+  
+  request(
+    { method: 'GET'
+    , uri: templateURL
+    , gzip: true
+    }
+  , function (error, response, body) {
+  	 	console.log('Baxel Hamet finished, now enter the project folder "' + program.new + '" and start hacking');
+    }
+  ).on('data', function(chunk) {
+    data.push(chunk);
+    dataLen += chunk.length;
+  })
+  .on('end', function() {
+    var buf = new Buffer(dataLen);
+  
+    for (var i=0, len = data.length, pos = 0; i < len; i++) { 
+        data[i].copy(buf, pos); 
+        pos += data[i].length; 
+    }
+    
+    var zip = new AdmZip(buf)
+      , _path = process.cwd() + '/tmp';
+    zip.extractAllTo (_path, true);
+    
+    var folder = fs.readdirSync(_path).filter(function(file) {
+      return fs.statSync(path.join(_path, file)).isDirectory();
+    });
+    
+    fs.renameSync(_path + '/' + folder[0], _path + '/../' + program.new);
+    fs.rmdirSync(_path);
+  })
+}
+
+// actions
+
+if (program.new) {
+  _new2();
+} else {
+  _run();
+}
