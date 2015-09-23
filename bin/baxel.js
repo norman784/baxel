9de7e74945fe0d5343a9a7c18928d89e216e678c
Norman Paniagua <normanpaniagua@gmail.com>
2015-09-23 00:01:55 -0400

    Updated bin

--- bin/baxel.js
+++ bin/baxel.js
@@ -1,18 +1,16 @@
 #!/usr/bin/env node
+"use strict";
 
-var fs = require('fs')
+let fs = require('fs')
   , program = require('commander')
   , path = require('path')
-  , mkdirp = require('mkdirp')
   , AdmZip = require('adm-zip')
-  , http = require('http')
-  , url = require('url')
   , nodemon = require('nodemon')
   , templateURL = require('../package.json')['baxel-template'];
 
 // Baxel options
 
-var options = {};
+let options = {};
 
 // options
 
@@ -20,8 +18,7 @@ program
   .version(require('../package.json').version)
   .usage('[options]')
   .option('run', 'run Baxel project on the current directory')
-  .option('new [name]', 'creates a new project on the folder with the same project name')
-  .option('env [development|production|custom]', 'Set app environment')
+  .option('new [name]', 'creates a new project on the folder with the same project name');
 
 program.on('--help', function(){
   console.log('  Usage:');
@@ -46,8 +43,8 @@ if (program.obj) {
  * Parse object either in `input` or in the file called `input`. The latter is
  * searched first.
  */
-function parseObj (input) {
-  var str, out;
+function parseObj () {
+  let str;
   try {
     str = fs.readFileSync(program.obj);
   } catch (e) {
@@ -66,29 +63,28 @@ function _run () {
     ext: 'js json'
   });
 
-  process.env.environment = program.env ? program.env : 'production';
-
   nodemon.on('start', function () {
-    console.log('Baxel [' + process.env.environment + '] server started');
+    console.log('Baxel server started');
   }).on('quit', function () {
-    console.log('Baxel [' + process.env.environment + '] has quit');
+    console.log('Baxel has quit');
   }).on('restart', function (files) {
-    console.log('Baxel [' + process.env.environment + '] restarted due to: ', files);
+    console.log('Baxel restarted due to: ', files);
   });
 }
 
 // new
 
 function _new () {
-  var request = require('request');
-  var data = [], dataLen = 0;
+  let request = require('request')
+    , data = []
+    , dataLen = 0;
 
   request(
     { method: 'GET'
     , uri: templateURL
     , gzip: true
     }
-  , function (error, response, body) {
+  , function () {
   	 	console.log('Baxel Hamet finished, now enter the project folder "' + program.new + '" and start hacking');
     }
   ).on('data', function(chunk) {
@@ -96,18 +92,18 @@ function _new () {
     dataLen += chunk.length;
   })
   .on('end', function() {
-    var buf = new Buffer(dataLen);
+    let buf = new Buffer(dataLen);
 
-    for (var i=0, len = data.length, pos = 0; i < len; i++) {
+    for (let i=0, len = data.length, pos = 0; i < len; i++) {
         data[i].copy(buf, pos);
         pos += data[i].length;
     }
 
-    var zip = new AdmZip(buf)
+    let zip = new AdmZip(buf)
       , _path = process.cwd() + '/tmp';
     zip.extractAllTo (_path, true);
 
-    var folder = fs.readdirSync(_path).filter(function(file) {
+    let folder = fs.readdirSync(_path).filter(function(file) {
       return fs.statSync(path.join(_path, file)).isDirectory();
     });
 
